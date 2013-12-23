800389352b373d76df4c76c9bb425be762a80722
carlosepp <carlosepp@gmail.com>
2013-12-23 17:43:57 -0600

    Flows

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,9 +2,16 @@ jQuery(document).ready(function(e){
 	var $ = jQuery;
 
 	$('.left-menu-custom ul.root > li.static > a.static.menu-item').click(function(event) {
-		$('.left-menu-custom ul.root > li.static > ul.static').slideUp(200);
-		$(this).parent().find('ul.static').slideToggle(200);
-		return false;
+
+			$('.left-menu-custom ul.root > li.static > ul.static')
+				.not($(this).parent().find('ul.static')).slideUp(200);
+			$(this).parent().find('ul.static').slideToggle(200);
+				
+
+		if($(this).attr('href').length >2){
+			return false;
+		}
+		
 	});
 	$('.left-menu-custom ul.root > li.static > ul.static').hide();
 
